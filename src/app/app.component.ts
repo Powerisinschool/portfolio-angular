c95022fb91586eb431b9fdb470fff5903f89ec38
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2024-12-20 22:00:22 +0100

    add preloader functionality and enhance cursor effects in home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'portfolio-angular';
+export class AppComponent implements AfterViewInit {
+  title = 'portfolio';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.setupPreloader();
+  }
+  setupPreloader(): void {
+    const preloader = document.getElementById('preloader');
+    if (!preloader) return;
+
+    setTimeout(() => {
+      this.renderer.addClass(preloader, 'fade-out');
+      setTimeout(() => {
+        this.renderer.setStyle(preloader, 'display', 'none');
+      }, 500);
+    }, 4000);
+  }
 }
