eacb2856f8950d5d3f1f4efa8d26aa0285e0f07f
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2024-12-21 21:28:39 +0100

    feat(project-card): implement project card component and integrate into home module

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -6,6 +6,8 @@ import {
   Renderer2,
 } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Project } from '../../../../shared/models/project.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +18,29 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   isWebLandscape = false;
+  // Initialize with a placeholder project in the array
+  projects$ = new Observable<Project[]>((observer) => {
+    observer.next([
+      {
+        id: 0,
+        title: 'Project One',
+        description: `A dynamic and interactive web application showcasing the ability to manage user profiles.`,
+        imageUrl:
+          'https://images.unsplash.com/photo-1722704264670-609ad35aaa53?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        projectUrl: '#',
+        tags: ['React', 'TypeScript', 'Firebase'],
+      },
+      {
+        id: 1,
+        title: 'Project Two',
+        description: `A comprehensive web application, showcasing a user-friendly interface for managing a blog.`,
+        imageUrl:
+          'https://images.unsplash.com/photo-1627534640397-561c6d443171',
+        projectUrl: '#',
+        tags: ['Python', 'Django', 'REST API'],
+      },
+    ]);
+  });
 
   constructor(
     private renderer: Renderer2,
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { TabletComponent } from './components/tablet/tablet.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, TabletComponent],
   providers: [],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
